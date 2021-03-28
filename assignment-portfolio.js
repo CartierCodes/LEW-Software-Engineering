ee36807e7f4eba126218c9450386656257ff5785
CartierCodes <cburz@att.net>
2021-03-28 17:29:57 -0500

    changes

--- assignment-portfolio.js
+++ assignment-portfolio.js
@@ -5,9 +5,6 @@ const port = process.env.PORT || 3000
 app.use(express.static(__dirname + '/'));
 
 app.get('/', (req,res) => res.render('index'));
-app.get('/dice-roller', (req,res) => res.render('dice-roller'));
-app.get('/course-syllabus', (req,res) => res.render('course-syllabus'));
-app.get('/regexp', (req,res) => res.render('regexp'));
 
 app.listen(port, () => console.log(`server started on port ${port}; ` +
     'press Ctrl-C to terminate....'))
